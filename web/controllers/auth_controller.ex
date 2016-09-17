5d5869f9d006e14633ec406417d4785a6c86ad77
Shia <rise.shia@gmail.com>
2016-09-17 16:38:09 +0900

    Rename UserFromAuth to User

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -26,7 +26,7 @@ defmodule Handsup.AuthController do
   end
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
-    case UserFromAuth.find_or_create(auth) do
+    case User.find_or_create(auth) do
       {:ok, user} ->
         conn
         |> put_flash(:info, "Successfully authenticated.")
