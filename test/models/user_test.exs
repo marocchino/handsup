07060262e6913e45745fd09dd1de9c6e22bc544d
Shia <rise.shia@gmail.com>
2016-09-17 18:25:41 +0900

    Implement User.find_or_create with test

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,7 +3,9 @@ defmodule Handsup.UserTest do
 
   alias Handsup.User
 
-  @valid_attrs %{nickname: "some content", provider: "some content", uid: "some content"}
+  @valid_attrs %{nickname: "some content",
+                 provider: "some content",
+                 uid: "some content"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
@@ -14,5 +16,33 @@ defmodule Handsup.UserTest do
   test "changeset with invalid attributes" do
     changeset = User.changeset(%User{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "find_or_create return {:ok, user} with exist user" do
+    auth = %{uid: "uid", provider: :google}
+    user_attrs = %{uid: "uid", provider: "google"}
+    changeset = User.changeset(%User{}, user_attrs)
+    {:ok, expected} = Repo.insert(changeset)
+
+    {status, actual} = User.find_or_create(auth)
+
+    assert status == :ok
+    assert expected == actual
+  end
+
+  test "find_or_create return {:ok, user} with new user" do
+    auth = %{uid: "uid", provider: :google}
+    {status, actual} = User.find_or_create(auth)
+
+    assert status == :ok
+    assert auth.uid == actual.uid
+    assert to_string(auth.provider) == actual.provider
+  end
+
+  test "find_or_create return {:error, _reason}" do
+    auth = %{uid: "", provider: :google}
+    {status, _reason} = User.find_or_create(auth)
+
+    assert status == :error
   end
 end
