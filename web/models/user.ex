bdcfbddb2292b1307f3ac1d325d42dfb17cdace0
Shim Won <marocchino@gmail.com>
2016-09-17 16:24:49 +0900

    oops

---
+++ web/models/user.ex
@@ -0,0 +1,21 @@
+defmodule Handsup.User do
+  use Handsup.Web, :model
+
+  schema "users" do
+    field :uid, :string
+    field :provider, :string
+    field :nickname, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:uid, :provider, :nickname])
+    |> validate_required([:uid, :provider])
+    |> unique_constraint(:uid)
+  end
+end
