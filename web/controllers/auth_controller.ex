f730ea3ace0b769c29ac1cc368017eaff7ade62e
Shia <rise.shia@gmail.com>
2016-10-01 15:01:18 +0900

    fix server could not recognized logined user

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -30,7 +30,7 @@ defmodule Handsup.AuthController do
       {:ok, user} ->
         conn
         |> put_flash(:info, "Successfully authenticated.")
-        |> put_session(:current_user, user)
+        |> put_session(:user_id, user.id)
         |> redirect(to: "/")
       {:error, reason} ->
         conn
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule Handsup.PageController do
   use Handsup.Web, :controller
 
   def index(conn, _params) do
-    user = get_session(conn, :current_user)
+    user = conn.assigns.current_user
     render conn, "index.html", user: user
   end
 end
