18fe0a2b3073f95902b0caa50100f86594435061
Shia <rise.shia@gmail.com>
2016-11-06 19:05:30 +0900

    chat implement prototype

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -1,8 +1,9 @@
 defmodule Handsup.UserSocket do
   use Phoenix.Socket
+  @max_age 7 * 24 * 60 * 60
 
   ## Channels
-  # channel "room:*", Handsup.RoomChannel
+  channel "event:*", Handsup.EventChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
@@ -19,9 +20,15 @@ defmodule Handsup.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket) do
+    case Phoenix.Token.verify(socket, "user socket", token, max_age: @max_age) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} ->
+        :error
+    end
   end
+  def connect(_params, _socket), do: :error
 
   # Socket id's are topics that allow you to identify all sockets for a given
   # user:
@@ -34,5 +41,5 @@ defmodule Handsup.UserSocket do
   #     Handsup.Endpoint.broadcast("users_socket:#{user.id}", "disconnect", %{})
   #
   # Returning `nil` makes this socket anonymous.
-  def id(_socket), do: nil
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
 end
